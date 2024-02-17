bab1c9e6b98828b4e002c52ba53286bc8506a76f
Jorge Gidi <jorgegidi@gmail.com>
2024-02-17 21:12:20 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='cspsa',
+    version='0.1.0',
+    description='Complex Simultaneous Perturbation Stochastic Approximation',
+    url='https://github.com/jgidi/cspsa.py',
+    author='Jorge Gidi',
+    author_email='jorgegidi@gmail.com',
+    license='Apache 2.0',
+    install_requires=['numpy', 'tqdm'],
+)
