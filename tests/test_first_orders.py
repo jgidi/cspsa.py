208d2bf5669f35f79d1198bb61895763ef14e4e5
Jorge Gidi <jorgegidi@gmail.com>
2024-03-08 16:25:02 +0100

    add tests for first order optimizers

---
+++ tests/test_first_orders.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import unittest
+
+from cspsa import SPSA, CSPSA
+from cspsa.defaults import *
+
+from .tools import naive_first_order
+
+
+def complex_objfun(x):
+    return np.abs(x[0] - 1 - 3j) + np.abs(x[1])
+
+def real_objfun(x):
+    return (x[0] - 4)**2 + (x[1] - 0.5)**2
+
+class FirstOrder(unittest.TestCase):
+
+    def test_spsa(self):
+        objfun = real_objfun
+        guess = np.random.randn(2)# + 1j * np.random.randn(2)
+
+        opt = SPSA()
+        params1 = opt.make_params_collector()
+
+        np.random.seed(0)
+        opt.run(objfun, guess)
+
+        np.random.seed(0)
+        params2 = naive_first_order(objfun, guess,
+                                    accumulate=True,
+                                    perturbations=DEFAULT_REAL_PERTURBATIONS)
+
+        for x1, x2 in zip(params1, params2):
+            self.assertTrue(np.all(x1 == x2))
+
+    def test_cspsa(self):
+        objfun = complex_objfun
+        guess = np.random.randn(2)
+
+        opt = CSPSA()
+        params1 = opt.make_params_collector()
+
+        np.random.seed(0)
+        opt.run(objfun, guess)
+
+        np.random.seed(0)
+        params2 = naive_first_order(objfun, guess,
+                                    accumulate=True,
+                                    perturbations=DEFAULT_COMPLEX_PERTURBATIONS)
+
+        for x1, x2 in zip(params1, params2):
+            self.assertTrue(np.all(x1 == x2))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/tools.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from cspsa.defaults import *
+
+def naive_first_order(f, guess,
+                      num_iter = DEFAULT_NUM_ITER,
+                      perturbations = DEFAULT_COMPLEX_PERTURBATIONS,
+                      gains = DEFAULT_GAINS,
+                      accumulate = False,
+                      ):
+
+
+    params = np.copy(guess)
+
+    if accumulate:
+        acc = []
+
+    for k in range(num_iter):
+        ak = gains['a'] / (k + gains['A'] + 1)**gains['s']
+        bk = gains['b'] / (k + 1) ** gains['t']
+
+        delta = bk * np.random.choice(perturbations, params.shape)
+
+        df = f(params + delta) - f(params - delta)
+
+        params = params - 0.5 * ak * df / delta.conj()
+
+        if accumulate:
+            acc.append(params)
+
+    if accumulate:
+        return acc
+    else:
+        return params
